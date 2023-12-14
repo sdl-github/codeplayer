a6d5f4e0b7522d0a62e52b7fd5836bdde3da0d80
sdl <sdl@192.168.10.102>
2023-12-14 19:33:43 +0800

    refactor

---
+++ packages/core/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <CodePlayer class="code-player"> </CodePlayer>
+    </div>
+</template>
+
+<script setup lang="ts">
+import CodePlayer from "./index";
+</script>
+
+
+<style lang="scss" scoped>
+.container {
+    width: 100%;
+    height: 100vh;
+    .code-player {
+        width: 100%;
+        height: 100%;
+    }
+}
+</style>
--- packages/docs/reference/support.md
+++
@@ -1,8 +0,0 @@
-# 支持语法
-
-- ✅ html
-- ✅ js/ts
-- ✅ css/less/scss
-- ✅ jsx/tsx
-- ✅ vue2/3
-- ✅ react
